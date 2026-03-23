abaedd5b3363bd39342fef67f3a77ab6a6d6a1f4
Abdinasir <Hogamiye2030@gmail.com>
2026-03-23 10:14:28 +0300

    waxaan kussodaray backend menu food

--- Backend/Controller/Foodcontroller.js
+++ Backend/Controller/Foodcontroller.js
@@ -5,7 +5,7 @@ const create = async (req, res) => {
     const newitem = await modelschuma.create({
       name: req.body.name,
       price: req.body.price,
-      image: req.file ? req.file.filename : "", // ✅ FIX
+      image: req.file ? req.file.filename : "", 
       category: req.body.category
     });
 
@@ -17,4 +17,53 @@ const create = async (req, res) => {
   }
 };
 
-module.exports = { create };
+const getAll=async(req,res)=>{
+  try{
+    const Read=await modelschuma.find();
+    res.status(201).json(Read);
+  }catch(err){
+     res.status(401).json({message : err.message})
+  }
+}
+const getbyid=async (req,res)=>{
+  try{
+    const ReadbyId=await modelschuma.findById(req.params.id)
+    res.status(201).json(ReadbyId)
+  }catch(err){
+    res.status(404).json({message:"User Not fond"})
+  }
+}
+
+const Deleteuser=async(req,res)=>{
+  try{
+    const deleteuser=await modelschuma.findByIdAndDelete(req.params.id);
+    res.status(201).json({message:"User deleted successfully"})
+  }catch(err){
+    res.status(404).json({message:"user not fond"})
+  }
+}
+
+const Updatefoodt=async (req,res)=>{
+    try{
+        const Updatefods=await modelschuma.findByIdAndUpdate(req.params.id,req.body)
+        res.status(201).json(Updatefods)
+    }catch(err){
+        console.log(err)
+    }if(req.file){
+        try{
+        const Updatefod=await modelschuma.findByIdAndUpdate(req.params.id,{image:req.file.filename},{new:true})
+        res.status(201).json(Updatefod)
+    }catch(err){
+        console.log(err)
+    }
+    }
+
+}
+
+module.exports = { 
+  create,
+  getAll,
+  getbyid,
+  Deleteuser,
+Updatefoodt
+ };
--- Backend/Router/Food.js
+++ Backend/Router/Food.js
@@ -5,5 +5,9 @@ const food=require("../Controller/Foodcontroller");
 const uploadimage = require("../Middleware/uploadimage");  
 
 router.post("/", uploadimage.single("image"), food.create);
+router.get("/", food.getAll);
+router.get("/:id",food.getbyid);
+router.delete("/:id", food.Deleteuser)
+router.put("/:id", uploadimage.single("image"), food.Updatefoodt);
 
 module.exports = router;
